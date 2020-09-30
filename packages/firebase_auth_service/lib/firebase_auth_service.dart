0cc4eefdb29fcceae51db350a673e98a991b3fb1
Josh Adams <joshua.william.adams@gmail.com>
2020-09-30 11:32:37 +0800

    temporarily disable facebook login due to firebase_core errors

--- packages/firebase_auth_service/lib/firebase_auth_service.dart
+++ packages/firebase_auth_service/lib/firebase_auth_service.dart
@@ -6,7 +6,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+// import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:http/http.dart' as http;
 import 'connect_credentials_model.dart';
 import 'package:flutter/foundation.dart';
@@ -17,7 +17,7 @@ class AuthService<T> {
   // generate instance of firebase authentication
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
   // 3rd party authentication module instances
-  final fb = FacebookLogin();
+  // final fb = FacebookLogin();
   final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   // create new stream that checks for authorisation state changes AND fetches
@@ -208,88 +208,88 @@ class AuthService<T> {
     }
   }
 
-  Future<void> signInWithFacebook({
-    AuthCredential authCredentialtoLink,
-  }) async {
-    // hold the instance of the authenticated firebase user
-    User user;
+  // Future<void> signInWithFacebook({
+  //   AuthCredential authCredentialtoLink,
+  // }) async {
+  //   // hold the instance of the authenticated firebase user
+  //   User user;
 
-    // Force the users to login using the login dialog based on WebViews.
-    fb.loginBehavior = FacebookLoginBehavior.webViewOnly;
+  //   // Force the users to login using the login dialog based on WebViews.
+  //   fb.loginBehavior = FacebookLoginBehavior.webViewOnly;
 
-    // log user out of existing facebook account
-    fb.logOut();
+  //   // log user out of existing facebook account
+  //   fb.logOut();
 
-    // commence the facebook authorisation using the OAuth standard
-    final result = await fb.logIn(['email']);
+  //   // commence the facebook authorisation using the OAuth standard
+  //   final result = await fb.logIn(['email']);
 
-    switch (result.status) {
-      case FacebookLoginStatus.loggedIn:
-        final FacebookAccessToken accessToken = result.accessToken;
-        // print('''
-        //  Logged in!
+  //   switch (result.status) {
+  //     case FacebookLoginStatus.loggedIn:
+  //       final FacebookAccessToken accessToken = result.accessToken;
+  //       // print('''
+  //       //  Logged in!
 
-        //  Token: ${accessToken.token}
-        //  User id: ${accessToken.userId}
-        //  Expires: ${accessToken.expires}
-        //  Permissions: ${accessToken.permissions}
-        //  Declined permissions: ${accessToken.declinedPermissions}
-        //  ''');
+  //       //  Token: ${accessToken.token}
+  //       //  User id: ${accessToken.userId}
+  //       //  Expires: ${accessToken.expires}
+  //       //  Permissions: ${accessToken.permissions}
+  //       //  Declined permissions: ${accessToken.declinedPermissions}
+  //       //  ''');
 
-        AuthCredential credential =
-            FacebookAuthProvider.credential(accessToken.token);
-        try {
-          user = (await _firebaseAuth.signInWithCredential(credential)).user;
-          // if authorisation credetials from a different provider passed. Link them.
-          if (user != null && authCredentialtoLink != null) {
-            user.linkWithCredential(authCredentialtoLink);
-          }
-        } catch (error) {
-          if (error is PlatformException &&
-              error.code == "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL") {
-            // fetch users details from facebook graph api
-            var graphResponse = await http.get(
-                'https://graph.facebook.com/v2.12/me?fields=name,first_name,last_name,email&access_token=${accessToken.token}');
-            // parse json response to object
-            Map<String, dynamic> map = jsonDecode(graphResponse.body);
-            // get email address
-            String email = map['email'];
-            // fetch providers for the email
-            List<String> providers =
-                await _firebaseAuth.fetchSignInMethodsForEmail(email);
-            // store details for linking credentials
-            LinkCredentials linkCredentials = LinkCredentials(
-              email: email,
-              providers: providers,
-              credentialToLink: credential,
-            );
-            // throw exception so link with credentials page can be generated
-            throw PlatformException(
-              code: "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL",
-              message:
-                  "Account exists with different credentials. Please log in with the correct account provider.",
-              details: linkCredentials,
-            );
-          }
-          // re throw error to allow catching by other functions
-          rethrow;
-        }
-        break;
-      case FacebookLoginStatus.cancelledByUser:
-        throw PlatformException(
-          code: 'FB_USER_CANCELED_LOGIN',
-          message: 'Login cancelled by the user.',
-        );
-        break;
-      case FacebookLoginStatus.error:
-        throw PlatformException(
-          code: 'FB_UNKNOWN_LOGIN_ERROR',
-          message: 'Something went wrong with the login process.\n'
-              'Here\'s the error Facebook gave us: ${result.errorMessage}',
-        );
-        break;
-    }
-  }
+  //       AuthCredential credential =
+  //           FacebookAuthProvider.credential(accessToken.token);
+  //       try {
+  //         user = (await _firebaseAuth.signInWithCredential(credential)).user;
+  //         // if authorisation credetials from a different provider passed. Link them.
+  //         if (user != null && authCredentialtoLink != null) {
+  //           user.linkWithCredential(authCredentialtoLink);
+  //         }
+  //       } catch (error) {
+  //         if (error is PlatformException &&
+  //             error.code == "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL") {
+  //           // fetch users details from facebook graph api
+  //           var graphResponse = await http.get(
+  //               'https://graph.facebook.com/v2.12/me?fields=name,first_name,last_name,email&access_token=${accessToken.token}');
+  //           // parse json response to object
+  //           Map<String, dynamic> map = jsonDecode(graphResponse.body);
+  //           // get email address
+  //           String email = map['email'];
+  //           // fetch providers for the email
+  //           List<String> providers =
+  //               await _firebaseAuth.fetchSignInMethodsForEmail(email);
+  //           // store details for linking credentials
+  //           LinkCredentials linkCredentials = LinkCredentials(
+  //             email: email,
+  //             providers: providers,
+  //             credentialToLink: credential,
+  //           );
+  //           // throw exception so link with credentials page can be generated
+  //           throw PlatformException(
+  //             code: "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL",
+  //             message:
+  //                 "Account exists with different credentials. Please log in with the correct account provider.",
+  //             details: linkCredentials,
+  //           );
+  //         }
+  //         // re throw error to allow catching by other functions
+  //         rethrow;
+  //       }
+  //       break;
+  //     case FacebookLoginStatus.cancelledByUser:
+  //       throw PlatformException(
+  //         code: 'FB_USER_CANCELED_LOGIN',
+  //         message: 'Login cancelled by the user.',
+  //       );
+  //       break;
+  //     case FacebookLoginStatus.error:
+  //       throw PlatformException(
+  //         code: 'FB_UNKNOWN_LOGIN_ERROR',
+  //         message: 'Something went wrong with the login process.\n'
+  //             'Here\'s the error Facebook gave us: ${result.errorMessage}',
+  //       );
+  //       break;
+  //   }
+  // }
 
   // ********************************** Sign Out methods **********************************
   Future<void> signOut() async {
