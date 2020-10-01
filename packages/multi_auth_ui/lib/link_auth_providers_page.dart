71dc18aa023209a48379b4251cd6887cf26c9919
Josh Adams <joshua.william.adams@gmail.com>
2020-10-01 15:03:09 +0800

    add_link_auth_providers_page

---
+++ packages/multi_auth_ui/lib/link_auth_providers_page.dart
@@ -0,0 +1,62 @@
+part of multi_auth_ui;
+
+class LinkAuthProvidersPage extends StatelessWidget {
+  final List<String> authProviders;
+  final Future Function() signInAnonymously;
+  final Future Function() signInWithGoogle;
+  final Future Function() signInWithFacebook;
+  final Future Function() signInWithApple;
+  final Function() emailSignInCallback;
+
+  LinkAuthProvidersPage({
+    @required this.authProviders,
+    this.signInAnonymously,
+    this.signInWithGoogle,
+    this.signInWithFacebook,
+    this.signInWithApple,
+    this.emailSignInCallback,
+  });
+
+  Widget _buildDescription(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: RichText(
+        text: TextSpan(
+          style: TextStyle(
+            color: Theme.of(context).textTheme.bodyText1.color,
+          ),
+          text:
+              'Account already exists using a different sign in method. Please sign in with one of the existing methods to link them.',
+        ),
+        textAlign: TextAlign.justify,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PageAppBar(
+        title: 'Link Authentication Credentials',
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          _buildDescription(context),
+          AuthOptions(
+            authOptionsTitle: 'Link Credentials',
+            authProviders: authProviders,
+            // never show separator
+            separatorLocation: null,
+            signInAnonymously: signInAnonymously,
+            signInWithApple: signInWithApple,
+            emailSignInCallback: emailSignInCallback,
+            signInWithGoogle: signInWithGoogle,
+            signInWithFacebook: signInWithFacebook,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- packages/multi_auth_ui/lib/multi_auth_ui.dart
+++ packages/multi_auth_ui/lib/multi_auth_ui.dart
@@ -13,3 +13,4 @@ part 'email_password_sign_in_model.dart';
 part 'email_password_sign_in_page.dart';
 part 'email_password_validator.dart';
 part 'auth_options_widget.dart';
+part 'link_auth_providers_page.dart';
