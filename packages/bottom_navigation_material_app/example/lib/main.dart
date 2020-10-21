3b646c1181e35fe51efe995a212120fcf63492f8
Josh Adams <joshua.william.adams@gmail.com>
2020-10-22 07:25:49 +0800

    bottom_navigation_material_app

---
+++ packages/bottom_navigation_material_app/example/lib/main.dart
@@ -0,0 +1,128 @@
+import 'package:bottom_navigation_material_app/bottom_navigation_material_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // define all navigation tabs for user
+  static final List<NavigationTabModel> navigationTabs = [
+    NavigationTabModel(
+      title: 'Home',
+      icon: Icons.home,
+      url: '/home',
+      navigatorKey: GlobalKey<NavigatorState>(),
+    ),
+    NavigationTabModel(
+      title: 'Search',
+      icon: Icons.search,
+      url: '/search',
+      navigatorKey: GlobalKey<NavigatorState>(),
+    ),
+    NavigationTabModel(
+      title: 'Profile',
+      icon: Icons.person,
+      url: '/profile',
+      navigatorKey: GlobalKey<NavigatorState>(),
+    ),
+  ];
+
+  // define route generator to be used for all tab navigator stacks
+  final RouteGenerator routeGenerator = RouteGenerator();
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationMaterialApp(
+      navigationTabs: navigationTabs,
+      generateRouteFunction: routeGenerator.generateRoute,
+    );
+  }
+}
+
+class RouteGenerator {
+  Route<dynamic> generateRoute(RouteSettings settings) {
+    // Widget builder (function that returns a widget) to construct the route page
+    WidgetBuilder builder;
+
+    // build different route (page) based on the route passed to the navigator
+    switch (settings.name) {
+      case '/home':
+        builder = (BuildContext context) {
+          return SamplePage(name: 'home');
+        };
+        break;
+      case '/search':
+        builder = (BuildContext context) {
+          return SamplePage(name: 'search');
+        };
+        break;
+      case '/profile':
+        builder = (BuildContext context) {
+          return SamplePage(name: 'profile');
+        };
+        break;
+      case '/':
+        builder = null;
+        break;
+      default:
+        // If there is no such named route in the switch statement
+        builder = (BuildContext context) {
+          return SamplePage();
+        };
+    }
+    // prevent page being added to default '/' route
+    if (builder == null) {
+      return null;
+    }
+    return MaterialPageRoute(
+      builder: builder,
+      settings: settings,
+    );
+  }
+}
+
+class SamplePage extends StatelessWidget {
+  final String name;
+
+  SamplePage({
+    this.name,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('$name'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            RaisedButton(
+              child: Text('push new route'),
+              onPressed: () {
+                Navigator.of(context).pushNamed('/$name');
+              },
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Text(
+                      index.toString(),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ packages/bottom_navigation_material_app/lib/bottom_navigation_material_app.dart
@@ -0,0 +1,114 @@
+library bottom_navigation_material_app;
+
+import 'package:flutter/material.dart';
+
+/// [BottomNavigationMaterialApp] is a material app at this core with the
+/// following additional functionality added:
+/// 1. Bottom navigation bar
+/// 2. Unique navigator for each bot nav bar item
+/// 3. Navigators all persist state
+class BottomNavigationMaterialApp extends StatefulWidget {
+  final List<NavigationTabModel> navigationTabs;
+  // route generator used to build all material page routes
+  final Route<dynamic> Function(RouteSettings settings) generateRouteFunction;
+
+  BottomNavigationMaterialApp({
+    @required this.navigationTabs,
+    @required this.generateRouteFunction,
+  });
+
+  @override
+  _BottomNavigationMaterialAppState createState() =>
+      _BottomNavigationMaterialAppState();
+}
+
+class _BottomNavigationMaterialAppState
+    extends State<BottomNavigationMaterialApp> {
+  /// set the current tab to the home page
+  int _currentIndex = 0;
+
+  void _select(int index) {
+    if (index == _currentIndex) {
+      /// case 1 - if user presses on currently selected tab
+      /// pop to first route - i.e. ensure no routes are over laid on top of the
+      /// current route
+      widget.navigationTabs[_currentIndex].navigatorKey.currentState
+          .popUntil((route) {
+        return route.isFirst;
+      });
+    } else {
+      // case 2 - user selects any other tab
+      // rebuild application state with the newly selected navigation tab
+      setState(() {
+        _currentIndex = index;
+      });
+    }
+  }
+
+  /// generate a list of navigators that will have their state persisted in an
+  /// indexed stack.
+  List<Widget> _getPersistantStack() {
+    return widget.navigationTabs.map((tab) {
+      return WillPopScope(
+        onWillPop: () async {
+          return !await tab.navigatorKey.currentState.maybePop();
+        },
+        child: Navigator(
+          key: tab.navigatorKey,
+          initialRoute: tab.url,
+          onGenerateRoute: widget.generateRouteFunction,
+        ),
+      );
+    }).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    /// ********************* HOLD POINT *********************
+    /// TODO - MaterialApp contains our top-level Navigator. This navigator is
+    /// required to enable navigation via urls in flutter web. Likely that this
+    /// section requires refractoring in some way to enable url updates from the
+    /// nested navigators and hyperlinking from web browsers to specific pages
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: IndexedStack(
+            children: _getPersistantStack(),
+            index: _currentIndex,
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: widget.navigationTabs.map((tab) {
+            return BottomNavigationBarItem(
+              label: tab.title,
+              icon: Icon(tab.icon),
+            );
+          }).toList(),
+          onTap: (int index) {
+            _select(index);
+          },
+          currentIndex: _currentIndex,
+          type: BottomNavigationBarType.fixed,
+          // hide titles on navigation bar
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+        ),
+      ),
+    );
+  }
+}
+
+class NavigationTabModel {
+  final String title;
+  final IconData icon;
+  final String url;
+  final GlobalKey<NavigatorState> navigatorKey;
+
+  NavigationTabModel({
+    this.title,
+    this.icon,
+    this.url,
+    this.navigatorKey,
+  });
+}
