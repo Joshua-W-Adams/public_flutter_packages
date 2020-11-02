f2344d32f029951a9829c6b2ad4a42932d45cb6c
Josh Adams <joshua.william.adams@gmail.com>
2020-11-03 06:36:37 +0800

    testing exmaple including url support
    bot nav bar, state persistance, url updating, deeplinking, browser forward, back and refresh buttons.

--- packages/bottom_navigation_material_app/example/lib/main.dart
+++ packages/bottom_navigation_material_app/example/lib/main.dart
@@ -1,132 +1,546 @@
-import 'package:bottom_navigation_material_app/bottom_navigation_material_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(NestedRouterDemo());
 }
 
-class MyApp extends StatelessWidget {
-  // define all navigation tabs for user
-  static final List<NavigationTabModel> navigationTabs = [
-    NavigationTabModel(
-      title: 'Home',
-      icon: Icons.home,
-      url: '/home',
-      navigatorKey: GlobalKey<NavigatorState>(),
-    ),
-    NavigationTabModel(
-      title: 'Search',
-      icon: Icons.search,
-      url: '/search',
-      navigatorKey: GlobalKey<NavigatorState>(),
-    ),
-    NavigationTabModel(
-      title: 'Profile',
-      icon: Icons.person,
-      url: '/profile',
-      navigatorKey: GlobalKey<NavigatorState>(),
-    ),
-  ];
+class Book {
+  final String title;
+  final String author;
 
-  // define route generator to be used for all tab navigator stacks
-  final RouteGenerator routeGenerator = RouteGenerator();
+  Book(this.title, this.author);
+}
+
+class NestedRouterDemo extends StatefulWidget {
+  @override
+  _NestedRouterDemoState createState() => _NestedRouterDemoState();
+}
+
+class _NestedRouterDemoState extends State<NestedRouterDemo> {
+  BookRouterDelegate _routerDelegate = BookRouterDelegate();
+  BookRouteInformationParser _routeInformationParser =
+      BookRouteInformationParser();
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationMaterialApp(
-      navigationTabs: navigationTabs,
-      generateRouteFunction: routeGenerator.generateRoute,
-      theme: ThemeData.dark(),
-      navigatorBuilder: (_, tabNavigator) {
-        return tabNavigator;
+    /// Construct material app using navigator 2.0.
+    return MaterialApp.router(
+      title: 'Books App',
+
+      /// routerDelegate is in charge of the state of the router application.
+      /// When the delegate detects an update in the route inforamtion parser it
+      /// updates the state of the material app.
+      routerDelegate: _routerDelegate,
+
+      /// routeInformationParser converts URL to a data model. This data model
+      /// is then passed to the routerDelegate
+      routeInformationParser: _routeInformationParser,
+    );
+  }
+}
+
+class BooksAppState extends ChangeNotifier {
+  int _selectedIndex;
+
+  Book _selectedBook;
+
+  final List<Book> books = [
+    Book('Stranger in a Strange Land', 'Robert A. Heinlein'),
+    Book('Foundation', 'Isaac Asimov'),
+    Book('Fahrenheit 451', 'Ray Bradbury'),
+  ];
+
+  BooksAppState() : _selectedIndex = 0;
+
+  int get selectedIndex => _selectedIndex;
+
+  set selectedIndex(int idx) {
+    _selectedIndex = idx;
+    notifyListeners();
+  }
+
+  Book get selectedBook => _selectedBook;
+
+  set selectedBook(Book book) {
+    _selectedBook = book;
+    notifyListeners();
+  }
+
+  int getSelectedBookById() {
+    if (!books.contains(_selectedBook)) return 0;
+    return books.indexOf(_selectedBook);
+  }
+
+  void setSelectedBookById(int id) {
+    if (id < 0 || id > books.length - 1) {
+      return;
+    }
+
+    _selectedBook = books[id];
+    notifyListeners();
+  }
+}
+
+/// Parses url route information to data of type [BookRoutePath]
+class BookRouteInformationParser extends RouteInformationParser<BookRoutePath> {
+  /// main function to convert information in url to [BookRoutePath] data type.
+  @override
+  Future<BookRoutePath> parseRouteInformation(
+      RouteInformation routeInformation) async {
+    final uri = Uri.parse(routeInformation.location);
+
+    if (uri.pathSegments.isNotEmpty && uri.pathSegments.first == 'settings') {
+      return BooksSettingsPath();
+    } else {
+      if (uri.pathSegments.length >= 2) {
+        if (uri.pathSegments[0] == 'book') {
+          return BooksDetailsPath(int.tryParse(uri.pathSegments[1]));
+        }
+      }
+      return BooksListPath();
+    }
+  }
+
+  /// restoreRouteInformation updates the browser history for the web
+  /// application.
+  @override
+  RouteInformation restoreRouteInformation(BookRoutePath configuration) {
+    if (configuration is BooksListPath) {
+      return RouteInformation(location: '/home');
+    }
+    if (configuration is BooksSettingsPath) {
+      return RouteInformation(location: '/settings');
+    }
+    if (configuration is BooksDetailsPath) {
+      return RouteInformation(location: '/book/${configuration.id}');
+    }
+    return null;
+  }
+}
+
+class BookRouterDelegate extends RouterDelegate<BookRoutePath>
+    with ChangeNotifier, PopNavigatorRouterDelegateMixin<BookRoutePath> {
+  final GlobalKey<NavigatorState> navigatorKey;
+
+  BooksAppState appState = BooksAppState();
+
+  BookRouterDelegate() : navigatorKey = GlobalKey<NavigatorState>() {
+    appState.addListener(notifyListeners);
+  }
+
+  @override
+  BookRoutePath get currentConfiguration {
+    if (appState.selectedIndex == 1) {
+      return BooksSettingsPath();
+    } else {
+      if (appState.selectedBook == null) {
+        return BooksListPath();
+      } else {
+        return BooksDetailsPath(appState.getSelectedBookById());
+      }
+    }
+  }
+
+  /// build function that is executed to rebuild the Navigator widget everytime
+  /// that a url change is detected and a new data model is output from the
+  /// information parser.
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      key: navigatorKey,
+      pages: [
+        MaterialPage(
+          child: AppShell(appState: appState),
+        ),
+      ],
+      onPopPage: (route, result) {
+        if (!route.didPop(result)) {
+          return false;
+        }
+
+        if (appState.selectedBook != null) {
+          appState.selectedBook = null;
+        }
+        notifyListeners();
+        return true;
+      },
+    );
+  }
+
+  /// handles the state of the router delegate when the user types a new url
+  /// into browser.
+  @override
+  Future<void> setNewRoutePath(BookRoutePath path) async {
+    if (path is BooksListPath) {
+      appState.selectedIndex = 0;
+      appState.selectedBook = null;
+    } else if (path is BooksSettingsPath) {
+      appState.selectedIndex = 1;
+    } else if (path is BooksDetailsPath) {
+      appState.selectedIndex = 0;
+      appState.setSelectedBookById(path.id);
+    }
+  }
+}
+
+// Routes
+abstract class BookRoutePath {}
+
+class BooksListPath extends BookRoutePath {}
+
+class BooksSettingsPath extends BookRoutePath {}
+
+class BooksDetailsPath extends BookRoutePath {
+  final int id;
+
+  BooksDetailsPath(this.id);
+}
+
+// Widget that contains the AdaptiveNavigationScaffold
+class AppShell extends StatefulWidget {
+  final BooksAppState appState;
+
+  AppShell({
+    @required this.appState,
+  });
+
+  @override
+  _AppShellState createState() => _AppShellState();
+}
+
+class _AppShellState extends State<AppShell> {
+  InnerRouterDelegate _routerDelegate;
+  ChildBackButtonDispatcher _backButtonDispatcher;
+
+  void initState() {
+    super.initState();
+    _routerDelegate = InnerRouterDelegate(widget.appState);
+  }
+
+  @override
+  void didUpdateWidget(covariant AppShell oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _routerDelegate.appState = widget.appState;
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    // Defer back button dispatching to the child router
+    _backButtonDispatcher = Router.of(context)
+        .backButtonDispatcher
+        .createChildBackButtonDispatcher();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = widget.appState;
+
+    // Claim priority, If there are parallel sub router, you will need
+    // to pick which one should take priority;
+    _backButtonDispatcher.takePriority();
+
+    return Scaffold(
+      appBar: AppBar(),
+      body: Router(
+        routerDelegate: _routerDelegate,
+        backButtonDispatcher: _backButtonDispatcher,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: 'Settings'),
+        ],
+        currentIndex: appState.selectedIndex,
+        onTap: (newIndex) {
+          appState.selectedIndex = newIndex;
+        },
+      ),
+    );
+  }
+}
+
+class InnerRouterDelegate extends RouterDelegate<BookRoutePath>
+    with ChangeNotifier, PopNavigatorRouterDelegateMixin<BookRoutePath> {
+  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+  BooksAppState get appState => _appState;
+  BooksAppState _appState;
+  set appState(BooksAppState value) {
+    if (value == _appState) {
+      return;
+    }
+    _appState = value;
+    notifyListeners();
+  }
+
+  InnerRouterDelegate(this._appState);
+
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      key: navigatorKey,
+      pages: [
+        if (appState.selectedIndex == 0) ...[
+          FadeAnimationPage(
+            child: BooksListScreen(
+              books: appState.books,
+              onTapped: _handleBookTapped,
+            ),
+            key: ValueKey('BooksListPage'),
+          ),
+          if (appState.selectedBook != null)
+            MaterialPage(
+              key: ValueKey(appState.selectedBook),
+              child: BookDetailsScreen(book: appState.selectedBook),
+            ),
+        ] else
+          FadeAnimationPage(
+            child: SettingsScreen(),
+            key: ValueKey('SettingsPage'),
+          ),
+      ],
+      onPopPage: (route, result) {
+        appState.selectedBook = null;
+        notifyListeners();
+        return route.didPop(result);
+      },
+    );
+  }
+
+  @override
+  Future<void> setNewRoutePath(BookRoutePath path) async {
+    // This is not required for inner router delegate because it does not
+    // parse route
+    assert(false);
+  }
+
+  void _handleBookTapped(Book book) {
+    appState.selectedBook = book;
+    notifyListeners();
+  }
+}
+
+class FadeAnimationPage extends Page {
+  final Widget child;
+
+  FadeAnimationPage({Key key, this.child}) : super(key: key);
+
+  Route createRoute(BuildContext context) {
+    return PageRouteBuilder(
+      settings: this,
+      pageBuilder: (context, animation, animation2) {
+        var curveTween = CurveTween(curve: Curves.easeIn);
+        return FadeTransition(
+          opacity: animation.drive(curveTween),
+          child: child,
+        );
       },
     );
   }
 }
 
-class RouteGenerator {
-  Route<dynamic> generateRoute(RouteSettings settings) {
-    // Widget builder (function that returns a widget) to construct the route page
-    WidgetBuilder builder;
+// Screens
+class BooksListScreen extends StatelessWidget {
+  final List<Book> books;
+  final ValueChanged<Book> onTapped;
 
-    // build different route (page) based on the route passed to the navigator
-    switch (settings.name) {
-      case '/home':
-        builder = (BuildContext context) {
-          return SamplePage(name: 'home');
-        };
-        break;
-      case '/search':
-        builder = (BuildContext context) {
-          return SamplePage(name: 'search');
-        };
-        break;
-      case '/profile':
-        builder = (BuildContext context) {
-          return SamplePage(name: 'profile');
-        };
-        break;
-      case '/':
-        builder = null;
-        break;
-      default:
-        // If there is no such named route in the switch statement
-        builder = (BuildContext context) {
-          return SamplePage();
-        };
-    }
-    // prevent page being added to default '/' route
-    if (builder == null) {
-      return null;
-    }
-    return MaterialPageRoute(
-      builder: builder,
-      settings: settings,
-    );
-  }
-}
-
-class SamplePage extends StatelessWidget {
-  final String name;
-
-  SamplePage({
-    this.name,
+  BooksListScreen({
+    @required this.books,
+    @required this.onTapped,
   });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('$name'),
+      body: ListView(
+        children: [
+          for (var book in books)
+            ListTile(
+              title: Text(book.title),
+              subtitle: Text(book.author),
+              onTap: () => onTapped(book),
+            )
+        ],
       ),
+    );
+  }
+}
+
+class BookDetailsScreen extends StatelessWidget {
+  final Book book;
+
+  BookDetailsScreen({
+    @required this.book,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(8.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            RaisedButton(
-              child: Text('push new route'),
+            FlatButton(
               onPressed: () {
-                Navigator.of(context).pushNamed('/$name');
+                Navigator.of(context).pop();
               },
+              child: Text('Back'),
             ),
-            SizedBox(
-              height: 16,
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: 100,
-                itemBuilder: (context, index) {
-                  return Card(
-                    child: Text(
-                      index.toString(),
-                    ),
-                  );
-                },
-              ),
-            ),
+            if (book != null) ...[
+              Text(book.title, style: Theme.of(context).textTheme.headline6),
+              Text(book.author, style: Theme.of(context).textTheme.subtitle1),
+            ],
           ],
         ),
       ),
     );
   }
 }
+
+class SettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Settings screen'),
+      ),
+    );
+  }
+}
+
+/// HOLD - Original code showcasing BottomNavigationMaterialApp Class
+
+// import 'package:bottom_navigation_material_app/bottom_navigation_material_app.dart';
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   // define all navigation tabs for user
+//   static final List<NavigationTabModel> navigationTabs = [
+//     NavigationTabModel(
+//       title: 'Home',
+//       icon: Icons.home,
+//       url: '/home',
+//       navigatorKey: GlobalKey<NavigatorState>(),
+//     ),
+//     NavigationTabModel(
+//       title: 'Search',
+//       icon: Icons.search,
+//       url: '/search',
+//       navigatorKey: GlobalKey<NavigatorState>(),
+//     ),
+//     NavigationTabModel(
+//       title: 'Profile',
+//       icon: Icons.person,
+//       url: '/profile',
+//       navigatorKey: GlobalKey<NavigatorState>(),
+//     ),
+//   ];
+
+//   // define route generator to be used for all tab navigator stacks
+//   final RouteGenerator routeGenerator = RouteGenerator();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return BottomNavigationMaterialApp(
+//       navigationTabs: navigationTabs,
+//       generateRouteFunction: routeGenerator.generateRoute,
+//       theme: ThemeData.dark(),
+//       navigatorBuilder: (_, tabNavigator) {
+//         return tabNavigator;
+//       },
+//     );
+//   }
+// }
+
+// class RouteGenerator {
+//   Route<dynamic> generateRoute(RouteSettings settings) {
+//     // Widget builder (function that returns a widget) to construct the route page
+//     WidgetBuilder builder;
+
+//     // build different route (page) based on the route passed to the navigator
+//     switch (settings.name) {
+//       case '/home':
+//         builder = (BuildContext context) {
+//           return SamplePage(name: 'home');
+//         };
+//         break;
+//       case '/search':
+//         builder = (BuildContext context) {
+//           return SamplePage(name: 'search');
+//         };
+//         break;
+//       case '/profile':
+//         builder = (BuildContext context) {
+//           return SamplePage(name: 'profile');
+//         };
+//         break;
+//       case '/':
+//         builder = null;
+//         break;
+//       default:
+//         // If there is no such named route in the switch statement
+//         builder = (BuildContext context) {
+//           return SamplePage();
+//         };
+//     }
+//     // prevent page being added to default '/' route
+//     if (builder == null) {
+//       return null;
+//     }
+//     return MaterialPageRoute(
+//       builder: builder,
+//       settings: settings,
+//     );
+//   }
+// }
+
+// class SamplePage extends StatelessWidget {
+//   final String name;
+
+//   SamplePage({
+//     this.name,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('$name'),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: Column(
+//           children: [
+//             RaisedButton(
+//               child: Text('push new route'),
+//               onPressed: () {
+//                 Navigator.of(context).pushNamed('/$name');
+//               },
+//             ),
+//             SizedBox(
+//               height: 16,
+//             ),
+//             Expanded(
+//               child: ListView.builder(
+//                 itemCount: 100,
+//                 itemBuilder: (context, index) {
+//                   return Card(
+//                     child: Text(
+//                       index.toString(),
+//                     ),
+//                   );
+//                 },
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
