b13e2fd9d15693a1abc9839e1d2ea2baaeeb6eac
Josh Adams <joshua.william.adams@gmail.com>
2021-05-13 08:38:50 +0800

    themechanger service updates
    null safety migration and test selection color to contrast primary color

--- packages/theme_changer_service/lib/theme_changer_service.dart
+++ packages/theme_changer_service/lib/theme_changer_service.dart
@@ -3,59 +3,100 @@ library theme_changer_service;
 import 'package:flutter/material.dart';
 
 class ThemeConfiguration {
-  final Color primaryColor;
-  final Color accentColor;
+  final String baseTheme;
+  final Color? primaryColor;
+  final Color? accentColor;
 
   ThemeConfiguration({
-    @required this.primaryColor,
-    @required this.accentColor,
-  });
+    required this.baseTheme,
+    this.primaryColor,
+    this.accentColor,
+  }) : assert(
+          ['light', 'dark'].contains(baseTheme),
+          'base theme must be light or dark',
+        );
 }
 
 class ThemeChangerService with ChangeNotifier {
   final String name;
-  ThemeData _themeData;
-  String _themeName;
 
-  ThemeChangerService({this.name}) {
+  ThemeChangerService({required this.name}) {
     generateTheme(name);
   }
 
-  Map<String, Object> _themeMap = {
+  ThemeData? _themeData;
+  String? _themeName;
+
+  Map<String, ThemeConfiguration> _supportedThemes = {
+    'Light': ThemeConfiguration(
+      baseTheme: 'light',
+    ),
+    'Dark': ThemeConfiguration(
+      baseTheme: 'dark',
+    ),
     'Dark Pink': ThemeConfiguration(
+      baseTheme: 'light',
       primaryColor: HexColor.fromHex('#870d4c'),
       accentColor: HexColor.fromHex('#eeff41'),
     ),
     'Dark Teal': ThemeConfiguration(
+      baseTheme: 'light',
       primaryColor: HexColor.fromHex('#006064'),
       accentColor: HexColor.fromHex('#a7ffeb'),
     ),
   };
 
+  ThemeData _generateTheme(ThemeConfiguration themeConfiguration) {
+    Color? primaryColour = themeConfiguration.primaryColor;
+    Color? textSelectionColor = primaryColour != null
+        ? ColorFunctions.getContrastingShadeColor(primaryColour)
+        : null;
+
+    if (themeConfiguration.baseTheme == 'light') {
+      return ThemeData.light().copyWith(
+        primaryColor: primaryColour,
+        accentColor: themeConfiguration.accentColor,
+        textSelectionTheme: textSelectionColor != null
+            ? TextSelectionThemeData(
+                cursorColor: textSelectionColor,
+                selectionColor: textSelectionColor,
+                selectionHandleColor: textSelectionColor,
+              )
+            : null,
+      );
+    }
+    return ThemeData.dark().copyWith(
+      primaryColor: primaryColour,
+      accentColor: themeConfiguration.accentColor,
+      textSelectionTheme: textSelectionColor != null
+          ? TextSelectionThemeData(
+              cursorColor: textSelectionColor,
+              selectionColor: textSelectionColor,
+              selectionHandleColor: textSelectionColor,
+            )
+          : null,
+    );
+  }
+
   void generateTheme(String themeName) {
-    ThemeConfiguration config = _themeMap[themeName];
+    ThemeConfiguration? config = _supportedThemes[themeName];
     this._themeName = themeName;
     if (config != null) {
-      this._themeData = ThemeData.light().copyWith(
-        primaryColor: config.primaryColor,
-        accentColor: config.accentColor,
-      );
-    } else if (themeName == 'Dark') {
-      this._themeData = ThemeData.dark();
+      this._themeData = _generateTheme(config);
     } else {
       this._themeData = ThemeData.light();
     }
   }
 
-  getThemeName() {
+  String? getThemeName() {
     return this._themeName;
   }
 
-  getTheme() {
+  ThemeData? getTheme() {
     return _themeData;
   }
 
-  setTheme(String themeName) {
+  void setTheme(String themeName) {
     generateTheme(themeName);
     // notifiy listeners to force a rebuild
     notifyListeners();
@@ -78,5 +119,43 @@ extension HexColor on Color {
         '${red.toRadixString(16).padLeft(2, '0')}'
         '${green.toRadixString(16).padLeft(2, '0')}'
         '${blue.toRadixString(16).padLeft(2, '0')}';
+  }
+}
+
+abstract class ColorFunctions {
+  static Color getContrastingShadeColor(Color color) {
+    /// determine luminance (brightness). 0 = black, 1 = white.
+    double lum = color.computeLuminance();
+
+    /// percentage modifier for constrasting color brightness
+    double modifier = 0.5;
+
+    /// return values based on bg luminance
+    if (lum >= 0.75) {
+      /// case 1 - super bright color
+      modifier = 0.5;
+    } else if (lum >= 0.675) {
+      /// case 2 - bright color
+      modifier = 0.375;
+    } else if (lum >= 0.5) {
+      /// case 3 - somewhat bright color
+      modifier = 0.25;
+    } else if (lum >= 0.375) {
+      /// case 4 - somewhat dark color
+      modifier = 1.75;
+    } else if (lum >= 0.25) {
+      /// case 5 - dark color
+      modifier = 2.25;
+    } else {
+      /// case 6 - really dark color
+      modifier = 2.5;
+    }
+
+    return changeColorLightness(color, lum * modifier);
+  }
+
+  /// lightness. 0 = black, 1 = white.
+  static Color changeColorLightness(Color color, double lightness) {
+    return HSLColor.fromColor(color).withLightness(lightness).toColor();
   }
 }
