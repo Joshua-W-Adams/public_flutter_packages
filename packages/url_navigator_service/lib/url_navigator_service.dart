4337979d7f7db5ecb9fe23a8ffbddcd2a5ca6167
Josh Adams <joshua.william.adams@gmail.com>
2021-11-04 11:03:27 +0800

    up url_navigator_service to null safety

--- packages/url_navigator_service/lib/url_navigator_service.dart
+++ packages/url_navigator_service/lib/url_navigator_service.dart
@@ -6,39 +6,39 @@ class UrlNavigatorService {
   final GlobalKey<NavigatorState> navigatorKey;
 
   UrlNavigatorService({
-    @required this.navigatorKey,
+    required this.navigatorKey,
   });
 
   Future<dynamic> navigateTo(
     String routeName, {
-    Map<String, String> queryParams,
-    Object arguments,
+    Map<String, String>? queryParams,
+    Object? arguments,
   }) {
     if (queryParams != null) {
       routeName = Uri(path: routeName, queryParameters: queryParams).toString();
     }
-    return navigatorKey.currentState.pushNamed(
+    return navigatorKey.currentState!.pushNamed(
       routeName,
       arguments: arguments,
     );
   }
 
   void goBack() {
-    return navigatorKey.currentState.pop();
+    return navigatorKey.currentState?.pop();
   }
 }
 
 class RoutingData {
-  final String route;
-  final Map<String, String> _queryParameters;
+  final String? route;
+  final Map<String, String>? _queryParameters;
 
   RoutingData({
     this.route,
-    Map<String, String> queryParameters,
+    Map<String, String>? queryParameters,
   }) : _queryParameters = queryParameters;
 
   // override the [] operator
-  operator [](String key) => _queryParameters[key];
+  operator [](String key) => _queryParameters![key];
 }
 
 extension StringExtension on String {
