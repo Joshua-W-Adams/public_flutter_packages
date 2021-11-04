cc58b12f33cee043d62a9cb9dabc6a182a615b9c
Josh Adams <joshua.william.adams@gmail.com>
2021-11-04 10:39:08 +0800

    up to file picker to null safety

--- packages/file_picker_service/lib/file_picker_service.dart
+++ packages/file_picker_service/lib/file_picker_service.dart
@@ -1,39 +1,40 @@
 library file_picker_service;
 
 import 'dart:io';
-import 'package:flutter/foundation.dart';
 import 'package:image_picker/image_picker.dart';
 
 class FilePickerService {
   ImagePicker _imagePicker = ImagePicker();
 
   /// Returns a [File] object pointing to the image that was picked.
-  Future<File> pickImage({
-    @required ImageSource source,
+  Future<File?> pickImage({
+    required ImageSource source,
     int imageQuality = 100,
-    double maxHeight,
-    double maxWidth,
+    double? maxHeight,
+    double? maxWidth,
   }) async {
-    PickedFile pickedFile = await _imagePicker.getImage(
+    PickedFile? pickedFile = await _imagePicker.getImage(
       source: source,
       imageQuality: imageQuality,
       maxHeight: maxHeight,
       maxWidth: maxWidth,
     );
+    if (pickedFile == null) return null;
     return File(pickedFile.path);
   }
 
   /// Returns a [File] object pointing to the video that was picked.
-  Future<File> pickVideo({
-    @required ImageSource source,
+  Future<File?> pickVideo({
+    required ImageSource source,
     Duration maxDuration = const Duration(
       seconds: 30,
     ),
   }) async {
-    PickedFile pickedFile = await _imagePicker.getVideo(
+    PickedFile? pickedFile = await _imagePicker.getVideo(
       source: source,
       maxDuration: maxDuration,
     );
+    if (pickedFile == null) return null;
     return File(pickedFile.path);
   }
 }
